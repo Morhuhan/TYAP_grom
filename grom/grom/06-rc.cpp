2e42bbd12b04896144203c2221fd2b0f6228531d
Kirill Shabalin <87900147+Morhuhan@users.noreply.github.com>
2023-11-14 11:30:21 +0500

    Add files via upload

--- grom/grom/06-rc.cpp
+++ grom/grom/06-rc.cpp
@@ -7,18 +7,91 @@
 
 // ищет не цепные правила g1 и добавляет их в g2
 void nonunit_rules(grammar & g1, grammar & g2) {
+
+    int rule_count = g1.count();
+
+    for (int r = 1; r <= rule_count; r++) {
+
+        RULE rule = g1[r];
+
+        if (!rule.is_unit()) {
+            g2.rule_add_from(g1, rule);
+        }
+    }
 }
 
 // ищет цепочку нетерминала A
 void find_chain_for(grammar & g1, SSET & R) {
+
+    int rule_count = g1.count();
+
+    while (1) {
+
+        int fix = R.count();
+
+        for (int r = 1; r <= rule_count; r++) {
+
+            RULE rule = g1[r];
+
+            if (R.contains(rule[0])) {
+
+                int symbol_count = rule.count();
+                
+                for (int s = 1; s <= symbol_count; s++) {
+                    R.insert(rule[s]);
+                }
+            }
+        }
+
+        if (fix == R.count()) {
+            break;
+        }
+    }
 }
 
 // формирует новые не цепные правила
 void nonunit_rules_for(grammar & g2, grammar & g1, SSET R) {
+    
+    int r_count = R.count();
+
+    int rule_count = g2.count();
+
+    for (int s = 1; s <= r_count; s++) {
+
+        SYMB X = R[s];
+
+        for (int r = 1; r <= rule_count; r++) {
+
+            RULE rule = g2[r];
+
+            if (rule[0] == R[s]) {
+                rule[0] = R[1];
+                g2.rule_add_from(g1, rule);
+            }
+        }
+    }
 }
 
 // устраняет цепные правила
-void remove_units(grammar & g1, grammar & g2) {
+void remove_units(grammar& g1, grammar& g2) {
+
+    g2.set_start_from(g1);
+
+    // Удаляем цепные правила из g1
+    nonunit_rules(g1, g2);
+
+    SYMB last = g1.nont_count();
+
+    for (SYMB A = 1; A <= last; A++) {
+
+        SSET R = -A;
+
+        // Для каждого оставшегося нетерминала находим множество его цепных правил
+        find_chain_for(g1, R);
+
+        // Для каждого оставшегося нетерминала добавляем компенсирующие правила
+        nonunit_rules_for(g2, g1, R);
+    }
 }
 
 // точка входа в алгоритм
--- grom/grom/07-lr.cpp
+++ grom/grom/07-lr.cpp
@@ -16,16 +16,68 @@ void compile_rule(RULE rule_k, RULE rule_j, RULE & rule_new) {
 // замен€ет правило Ak->Ajx на правила Ak->wx, w - правые части Aj
 // k - номер правила Ak->Ajx
 void replace_Aj(int k, grammar & g1) {
+
+    RULE rule_k = g1[k];
+
+    SYMB Aj = rule_k[1];
+
+    RULE rule_new;
+
+    for (int j = 0; j < length; j++) {
+
+    }
+
 }
 
 // преобразует не€вную рекурсию в €вную
 // дл€ нетерминалов от первого до Ak
 // замен€ет правила вида Ak->Ajx на Ak->wx, w - правые части Aj
 void indirect_to_direct(SYMB Ak, grammar & g1) {
+
+    int rule_count = g1.count();
+
+    // ѕеребераем символы с индексом < k
+    for (SYMB Aj = 1; Aj < Ak ; Aj++) {
+
+        // ƒл€ каждого символа просматриваем все правила
+        for (int k = 0; k < rule_count; k++) {
+
+            RULE rule = g1[k];
+
+            // »щем такие правила, где слева исследуемый нетерминал Ak, а справа нетерминал
+            // с индексом j, который меньше по абсолютному значению, чем k
+            if ((rule[0] == -Ak) && (rule[1] == -Aj)) {
+
+                // «аемен€ем тело правила на тела правил дл€ Aj
+                replace_Aj(k, g1);
+            }
+        }
+    }
 }
 
 // устран€ет левую рекурсию
 void eliminate_leftr(grammar & g1) {
+
+    // »ндекс последнего нетерминала
+    SYMB last = g1.nont_count();
+
+    for (SYMB A = 1; ; A++) {
+
+        // ”стран€ем €вную рекурсию
+        if (g1.is_symbol_leftr(-A)) {
+            eliminate_direct(-A, g1);
+        }
+
+        // ”слови€ выхода
+        if (A == last) {
+            break;
+        }
+
+        // ”стран€ем не€вную рекурсию
+        else {
+            indirect_to_direct(A + 1, g1);
+        }
+    }
 }
 
 // точка входа в алгоритм
