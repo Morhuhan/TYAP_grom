eaa03d78d6a38a35d4b4fcd94631cf1fef8b9181
Kirill Shabalin <87900147+Morhuhan@users.noreply.github.com>
2024-03-03 18:12:45 +0500

    Add files via upload

--- grom/grom/09-cnf.cpp
+++ grom/grom/09-cnf.cpp
@@ -2,23 +2,106 @@
 // 09-cnf.cpp (приведение к нормальной форме Хомского)
 #include "stdafx.h"
 #include "grammar.h"
+#include <iostream>
 // командные строки
 // 002 -cnf -gr
 
 // правило для вывода терминала
 void term_rule(SYMB A, SYMB a, grammar & g1, grammar & g2) {
+
+    g2.reg_nont(g1.getid(A));
+    g2.reg_term(g1.getid(a));
+
+    RULE rule;
+
+    rule[0] = A;
+    rule.append(a);
+
+    g2.rule_add(rule);
 }
 
 // приводит правило к нормальной форме Хомского
 void rule_to_cnf(RULE rule_old, grammar & g1, grammar & g2) {
+
+    if (rule_old.count() == 2) {
+        g2.rule_add(rule_old);
+    }
+
+    else {
+
+        RULE rule_new;
+
+        rule_new[0] = rule_old[0];
+        rule_new.append(rule_old[1]);
+        rule_new.append(rule_old[2]);
+        g2.reg_nont(g1.getid(rule_old[1]));
+        g2.reg_nont(g1.getid(rule_old[2]));
+        g2.rule_add(rule_new);
+
+        // Удаляем первый символ rule_old
+        RULE temp_rule;
+        temp_rule[0] = rule_old[2];
+
+        for (int j = 2; j <= rule_old.count(); j++) {
+            temp_rule.append(rule_old[j]);
+        }
+
+        rule_old = temp_rule;
+
+        rule_to_cnf(rule_old, g1, g2);
+    }
 }
 
 // заменяет терминалы нетерминалами
 void terms_to_nonts(RULE & rule, grammar & g1, grammar & g2) {
+    
+    int symbol_count = rule.count();
+
+    for (int s = 1; s <= symbol_count; s++) {
+        SYMB symbol = rule[s];
+
+        if (symbol > 0) {
+            SYMB A = g1.get_new_nont();
+
+            rule[s] = A;
+
+            term_rule(A, symbol, g1, g2);
+        }
+    }
 }
 
 // приводит грамматику к нормальной форме Хомского
 int chomsky_normal_form(grammar & g1, grammar & g2) {
+
+    g2.set_start_from(g1);
+
+    int rule_count = g1.count();
+
+    for (int r = 1; r <= rule_count; r++) {
+
+        RULE rule = g1[r];
+
+        if (rule.count() == 0) {
+            if (rule[0] != g1.start()) {
+                throw "Unexpected empty rule";
+                return 0;
+            }
+            else {
+                g2.rule_add_from(g1, rule);
+            }
+        }
+
+        if (rule.count() > 1) {
+            terms_to_nonts(rule, g1, g2);
+            rule_to_cnf(rule, g1, g2);
+        }
+
+        if (rule.count() == 1) {
+            g2.reg_term(g1.getid(rule[1]));
+            g2.rule_add_from(g2, rule);
+        }
+    }
+
     return 1;
 }
 
--- grom/grom/10-faus.cpp
+++ grom/grom/10-faus.cpp
@@ -3,16 +3,139 @@
 #include "stdafx.h"
 #include "grammar.h"
 // командные строки
-// a13a -faus
+// a13b -faus
 // a10 -faus
 
 // ознакомительные действия с автоматом
 void fa_general(NFA & fa) {
+
+    SYMB A = fa.reg_state_id("A");
+    SYMB B = fa.reg_state_id("B");
+    SYMB C = fa.reg_state_id("C");
+    SYMB D = fa.reg_state_id("D");
+    SYMB E = fa.reg_state_id("E");
+    SYMB F = fa.reg_state_id("F");
+    SYMB G = fa.reg_state_id("G");
+
+    TRAN tran;
+    tran.a = A;
+    tran.b = B;
+    tran.c = '0';
+    fa.tran_add(tran);
+    
+    tran.a = A;
+    tran.b = C;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = B;
+    tran.b = D;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = C;
+    tran.b = E;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = D;
+    tran.b = C;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = D;
+    tran.b = E;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = E;
+    tran.b = B;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = E;
+    tran.b = D;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = F;
+    tran.b = D;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = G;
+    tran.b = F;
+    tran.c = '0';
+    fa.tran_add(tran);
+
+    tran.a = G;
+    tran.b = E;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    tran.a = G;
+    tran.b = F;
+    tran.c = '1';
+    fa.tran_add(tran);
+
+    fa.initials.insert(A);
+    fa.finals.insert(D);
+    fa.finals.insert(E);
 }
 
 // устранение недостижимых состояний конечного автомата
 void fa_remove_unreachable(NFA & fa1, NFA & fa2) {
-    fa_general(fa2);
+    
+    //fa_general(fa2);
+    
+    int tran_count = fa1.delta_count();
+
+    SSET R = fa1.initials;
+
+    for (int s = 1; s <= R.count(); s++) {
+        fa2.reg_initial_from(fa1, R[s]);
+    }
+
+    while (1) {
+
+        int flag = 0;
+
+        for (int t = 1; t <= tran_count; t++) {
+
+            TRAN tran = fa1[t];
+
+            if (R.contains(tran.a)) {
+
+                if (!R.contains(tran.b)) {
+                    R.insert(tran.b);
+                    flag = 1;
+                }
+                else
+                {
+                    R.insert(tran.b);
+                }
+            }
+        }
+
+        if (flag == 0) { break; }
+    }
+
+    int finals_count = fa1.finals.count();
+
+    for (int f = 1; f <= finals_count; f++) {
+        SYMB fin = fa1.finals[f];
+        if (R.contains(fin)) {
+            fa2.reg_final_from(fa1, fin);
+        }
+    }
+
+    for (int t = 1; t <= tran_count; t++) {
+        TRAN tran = fa1[t];
+        if (R.contains(tran.a) && R.contains(tran.b)) {
+            fa2.tran_add_from(fa1, tran);
+        }
+    }
+
 }
 
 // точка входа в алгоритм
