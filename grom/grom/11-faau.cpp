950324135f6b7967323766fe5dfeff186450eb3e
Kirill Shabalin <87900147+Morhuhan@users.noreply.github.com>
2024-03-10 19:26:49 +0500

    Add files via upload

--- grom/grom/11-faau.cpp
+++ grom/grom/11-faau.cpp
@@ -7,11 +7,47 @@
 
 // определяет наличие перехода из финального состояния
 int has_tran_from_final(SYMB A, NFA & fa) {
+    int delta_count = fa.delta_count();
+    for (int t = 1; t < delta_count; t++) {
+        TRAN transit = fa[t];
+        if (transit.a == A) { return 1; }
+    }
     return 0;
 }
 
 // преобразует автомат в грамматику
 void fa_to_grau(NFA & fa, grammar & gr) {
+
+    int delta_count = fa.delta_count();
+
+    for (int t = 1; t < delta_count; t++) {
+        TRAN transit = fa[t];
+
+        RULE rule;
+
+        if (fa.finals.contains(transit.b)) {
+            rule[0] = gr.reg_from(fa, transit.a);
+            rule.append(gr.reg_char(transit.c));
+            gr.rule_add(rule);
+
+            if (has_tran_from_final(transit.b, fa)) {
+                rule.append(gr.reg_from(fa, transit.b));
+                
+            }
+        }
+
+        else {
+            rule[0] = gr.reg_from(fa, transit.a);
+            rule.append(gr.reg_char(transit.c));
+            rule.append(gr.reg_from(fa, transit.b));
+        }
+
+        gr.rule_add(rule);
+    }
+
+    int initials_count = fa.initials.count();
+
+    gr.set_start(gr.reg_from(fa, fa.initials[1]));
 }
 
 // точка входа в алгоритм
--- grom/grom/12-aufa.cpp
+++ grom/grom/12-aufa.cpp
@@ -2,16 +2,81 @@
 // 12-aufa.cpp (преобразование грамматики в автомат)
 #include "stdafx.h"
 #include "grammar.h"
+#include <iostream>
 // командные строки
 // a11 -faau -aufa
 
+using namespace std;
+
 // находит пару A->a или A->aB, возвращает номер правила
-int has_pair_rule(RULE rule, int length, grammar gr) {
+int has_pair_rule(RULE rule, grammar gr) {
+
+    int rules_count = gr.count();
+
+    int rule_length = rule.count();
+
+    for (int i = 1; i <= rules_count; i++) {
+
+        RULE rule_i = gr[i];
+
+        if (rule_i[0] == rule[0] && rule_i[1] == rule[1] && rule_i.count() == 2) {
+
+            if (rule_i.count() > 1) {
+                return gr.rule_exist(rule_i);
+            }
+        }
+    }
+
     return 0;
 }
 
 // преобразует грамматику в автомат
 int grau_to_fa(grammar & gr, NFA & fa) {
+
+    fa.reg_initial_id(gr.getid(gr.start()));
+
+    int rules_count = gr.count();
+
+    for (int i = 1; i <= rules_count; i++) {
+
+        RULE rule = gr[i];
+
+        TRAN tran;
+
+        if (rule.count() > 2) {
+            cout << "Grammer is wrong";
+            return 0;
+        }
+
+        else if (rule.count() == 1) {
+
+            if (has_pair_rule(rule, gr) != 0) {
+                RULE pair_rule = gr[has_pair_rule(rule, gr)];
+
+                tran.a = fa.reg_state_from(gr, rule[0]);
+                tran.c = gr.get_char(rule[1]);
+                tran.b = fa.reg_final_from(gr, pair_rule[2]);
+            }
+
+            else
+            {
+                tran.a = fa.reg_state_from(gr, rule[0]);
+                tran.c = gr.get_char(rule[1]);
+                tran.b = fa.reg_final_from(gr, gr.get_new_nont());
+            }
+
+        }
+
+        else if (rule.count() == 2) {
+
+            tran.a = fa.reg_state_from(gr, rule[0]);
+            tran.c = gr.get_char(rule[1]);
+            tran.b = fa.reg_state_from(gr, rule[2]);
+        }
+
+        fa.tran_add(tran);
+    }
+
     return 1;
 }
 
